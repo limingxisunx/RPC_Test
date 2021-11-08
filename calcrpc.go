9db1448fd9212c8f00b937225b467d11377e8033
limingxisunx <77558982+limingxisunx@users.noreply.github.com>
2021-11-08 14:53:51 +0800

    加入代码

---
+++ calcrpc.go
@@ -0,0 +1,27 @@
+package RPC_Test
+
+import "net/rpc"
+
+// ServiceName 计算器服务名
+const ServiceName = "CalcService"
+
+// ServiceInterface 计算器服务接口
+type ServiceInterface interface {
+	// CalcTwoNumber 对两个数进行运算
+	CalcTwoNumber(request Calc, reply *float64)  error
+	// GetOperators 获取所有支持的运算
+	GetOperators(request struct{}, reply *[]string)  error
+}
+
+// RegisterCalcService register the RPC service on svc
+func RegisterCalcService(svc ServiceInterface) error {
+	return rpc.RegisterName(ServiceName, svc)
+}
+
+// Calc 定义计算器对象，包括两个运算数
+type Calc struct {
+	Number1 float64
+	Number2 float64
+	Operator string
+}
+
---
+++ server/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"net/http"
+	"net/rpc"
+	"server/calc"
+)
+
+/* RPC 服务实现 */
+
+// CalcService 是计算器 RPC 服务的实现
+type CalcService struct{}
+
+// CalcTwoNumber 对两个数进行加减乘除运算
+func (c *CalcService) CalcTwoNumber(request calc.Calc, reply *float64) error {
+	oper, err := CreateOperation(request.Operator)
+	if err != nil {
+		return err
+	}
+	*reply = oper(request.Number1, request.Number2)
+	return nil
+}
+
+// GetOperators 获取所有支持的运算
+func (c *CalcService) GetOperators(request struct{}, reply *[]string) error {
+	opers := make([]string, 0, len(Operators))
+	for key := range Operators {
+		opers = append(opers, key)
+	}
+	*reply = opers
+	return nil
+}
+
+/* 运行 RPC 服务 */
+
+func main() {
+	calc.RegisterCalcService(new(CalcService))
+	rpc.HandleHTTP()
+	http.ListenAndServe(":8080", nil)
+}
